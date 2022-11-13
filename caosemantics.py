f372f120d3a80953c9caf7ee38054315d4c08785
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2022-11-13 07:03:58 -0600

    Update caosemantics.py
    Updated file to main branch

--- caosemantics.py
+++ caosemantics.py
@@ -1,3 +1,7 @@
 # Display the output
 
-print("New Python file")
+import random
+words = ("elements", "molecules", "atoms", "protons", "electrons", "neutrons")
+word = random.choice(words)
+correct = word
+print(word)
